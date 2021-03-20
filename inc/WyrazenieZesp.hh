8a2fe088e89572c1bca3e6ef8b8a7cca4a6f5b63
Michał Szleger <259235@student.pwr.edu.pl>
2021-03-20 13:41:32 +0100

    Add module to handle complex expression.

--- inc/WyrazenieZesp.hh
+++ inc/WyrazenieZesp.hh
@@ -1,17 +1,12 @@
 #ifndef WYRAZENIEZESP_HH
 #define WYRAZENIEZESP_HH
 
-
-
 #include "LZespolona.hh"
 
-
-/*!
+/*
  * Modeluje zbior operatorow arytmetycznych.
  */
 enum Operator { Op_Dodaj, Op_Odejmij, Op_Mnoz, Op_Dziel };
-
-
 
 /*
  * Modeluje pojecie dwuargumentowego wyrazenia zespolonego
@@ -20,16 +15,21 @@ struct WyrazenieZesp {
   LZespolona   Arg1;   // Pierwszy argument wyrazenia arytmetycznego
   Operator     Op;     // Opertor wyrazenia arytmetycznego
   LZespolona   Arg2;   // Drugi argument wyrazenia arytmetycznego
+
+  /**
+   * Metoda zwraca wartość wyrażenia zespolonego
+  **/
+  LZespolona Oblicz();
 };
 
+/*
+ * Przeciążenie operatora strumienia wyjściowego <<
+ */
+std::ostream & operator << (std::ostream & s, WyrazenieZesp WyrZ);
 
 /*
- * Funkcje ponizej nalezy zdefiniowac w module.
- *
+ * Przeciążenie operatora strumienia wejściowego >>
  */
-
-
-void Wyswietl(WyrazenieZesp  WyrZ);
-LZespolona Oblicz(WyrazenieZesp  WyrZ);
+std::istream & operator >> (std::istream & is, WyrazenieZesp & WyrZ);
 
 #endif
--- src/WyrazenieZesp.cpp
+++ src/WyrazenieZesp.cpp
@@ -1,7 +1,63 @@
-#include "WyrazenieZesp.hh"
+#include "../inc/WyrazenieZesp.hh"
+
+std::ostream & operator << (std::ostream & s, WyrazenieZesp WyrZ)
+{
+    char znaki[] = {'+', '-', '*', '/'};
+    return s << WyrZ.Arg1 << " " << znaki[WyrZ.Op] << " " << WyrZ.Arg2;
+}
+
+std::istream & operator >> (std::istream & is, WyrazenieZesp & WyrZ)
+{
+    char znak;
+    // Wczytywanie pierwszego argumentu
+    is >> WyrZ.Arg1;
+    if(is.fail())
+        return is;
+    // Wczytanie znaku operacji
+    is >> znak;
+    if(is.fail())
+        return is;
+    if(znak == '+')
+        WyrZ.Op = Op_Dodaj;
+    else if(znak == '-')
+        WyrZ.Op = Op_Odejmij;
+    else if(znak == '*')
+        WyrZ.Op = Op_Mnoz;
+    else if(znak == '/')
+        WyrZ.Op = Op_Dziel;
+    else {
+        is.setstate(std::ios::failbit);
+        return is;
+    }
+    // Wczytywanie drugiego argumentu
+    is >> WyrZ.Arg2;
+    if(is.fail())
+        return is;
+    return is;
+}
+
+LZespolona WyrazenieZesp::Oblicz()
+{
+    // Obsługiwanie operacji dodawania
+    if(this->Op == 0)
+        return this->Arg1 + this->Arg2;
+    // Obsługiwanie operacji minus
+    if(this->Op == 1)
+        return this->Arg1 - this->Arg2;
+    // Obsługiwanie operacji mnożenia
+    if(this->Op == 2)
+        return this->Arg1 * this->Arg2;
+    // Obsługiwanie operacji dzielenia
+    if(this->Op == 3)
+        return this->Arg1 / this->Arg2;
+    throw (2);
+}
 
 
-/*
- * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
- * w pliku naglowkowym.
- */
+
+
+
+
+
+
+
