16c5389342c870ff50f263ac34b4b5e4315349a7
Michał Szleger <259235@student.pwr.edu.pl>
2021-03-20 13:42:39 +0100

    Add module to handle complex numbers.

--- inc/LZespolona.hh
+++ inc/LZespolona.hh
@@ -1,30 +1,89 @@
 #ifndef LZESPOLONA_HH
 #define LZESPOLONA_HH
 
-/*!
- *  Plik zawiera definicje struktury LZesplona oraz zapowiedzi
- *  przeciazen operatorow arytmetycznych dzialajacych na tej 
- *  strukturze.
- */
+#include <iostream>
 
-
-/*!
+/**
  * Modeluje pojecie liczby zespolonej
- */
+**/
 struct  LZespolona {
-  double   re;    /*! Pole repezentuje czesc rzeczywista. */
-  double   im;    /*! Pole repezentuje czesc urojona. */
+  double   re;    // Pole repezentuje część rzeczywistą.
+  double   im;    // Pole repezentuje część urojoną.
+
+  /**
+   * Konstruktor bezargumentowy ustawiający wartości zmiennych re i im na 0
+  **/
+  LZespolona();
+
+  /**
+   * Konstruktor pobierający dwa argumenty, które przypisuje zmiennym re i im (w tej kolejności)
+  **/
+  LZespolona(double reValue, double imValue);
+
+  /**
+   * Zwraca sprzężenie podanej liczby zespolonej
+  **/
+  LZespolona Sprzezenie();
+
+  /**
+   * Zwraca kwadrat modułu podanej liczby zespolonej
+  **/
+  double Modul2();
 };
 
+/*
+ * Przeciążenie operatora +: Dodawanie dwóch liczb zespolonych
+ */
+LZespolona operator + (LZespolona liczba1, LZespolona liczba2);
 
 /*
- * Dalej powinny pojawic sie zapowiedzi definicji przeciazen operatorow
+ * Przeciążenie operatora -: Odejmowanie dwóch liczb zespolonych
  */
+LZespolona operator - (LZespolona liczba1, LZespolona liczba2);
 
-bool  operator == (LZespolona  Skl1,  LZespolona  Skl2);
+/*
+ * Przeciążenie operatora *: Mnożenie dwóch liczb zespolonych
+ */
+LZespolona operator * (LZespolona liczba1, LZespolona liczba2);
 
-LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2);
+/*
+ * Przeciążenie operatora *: Mnożenie liczby zespolonej przez liczbę rzeczywistą
+ */
+LZespolona operator * (LZespolona liczba1, double liczba2);
 
-LZespolona operator / (LZespolona Skl1, double Skl2);
+/*
+ * Przeciążenie operatora *: Mnożenie liczby rzeczywistą przez liczbę zespoloną
+ */
+LZespolona operator * (double liczba1, LZespolona liczba2);
+
+/*
+ * Przeciążenie operatora /: Dzielenie dwóch liczb zespolonych
+ */
+LZespolona operator / (LZespolona liczba1, LZespolona liczba2);
+
+/*
+ * Przeciążenie operatora /: Dzielenie liczby zespolonej przez liczbę rzeczywistą
+ */
+LZespolona operator / (LZespolona liczba1, double liczba2);
+
+/*
+ * Przeciążenie operatora ==: Porównanie dwóch liczb zespolonych (1 - takie same / 0 - różne)
+ */
+bool operator == (LZespolona liczba1, LZespolona liczba2);
+
+/*
+ * Przeciążenie operatora !=: Porównania dwóch liczb zespolonych (0 - takie same / 1 - różne)
+ */
+bool operator != (LZespolona liczba1, LZespolona liczba2);
+
+/*
+ * Przeciążenie operatora strumienia wyjściowego <<
+ */
+std::ostream & operator << (std::ostream & s, LZespolona liczba);
+
+/*
+ * Przeciążenie operatora strumienia wejściowego >>
+ */
+std::istream& operator >> (std::istream& is, LZespolona& liczba);
 
 #endif
--- src/LZespolona.cpp
+++ src/LZespolona.cpp
@@ -1,60 +1,154 @@
-#include "LZespolona.hh"
-#include <cmath>
+#include "../inc/LZespolona.hh"
 
-#define MIN_DIFF 0.00001
-
-/*!
- * Realizuje porównanie dwoch liczb zespolonych.
- * Argumenty:
- *    Skl1 - pierwsza porównywana liczba zespolona,
- *    Skl2 - druga porównywana liczba zespolona.
- * Zwraca:
- *    True dla równych liczb zespolonych.
- */
-
-bool  operator == (LZespolona  Skl1,  LZespolona  Skl2){
-  if ((Skl1.re == Skl2.re) && (Skl1.im == Skl2.im))
-    return true;
-  else
-    return false;
-  //alternatywnie, dla MIN_DIFF i wyników od użytkownika
-  /*
-  if abs(Skl1.re - Skl2.re) <= MIN_DIFF && abs(Skl1.im - Skl2.im) <= MIN_DIFF
-    return true;
-  else
-    return false;
-  */
+LZespolona  operator + (LZespolona  liczba1,  LZespolona  liczba2)
+{
+  liczba1.re += liczba2.re;
+  liczba1.im += liczba2.im;
+  return liczba1;
 }
 
-/*!
- * Realizuje dodanie dwoch liczb zespolonych.
- * Argumenty:
- *    Skl1 - pierwszy skladnik dodawania,
- *    Skl2 - drugi skladnik dodawania.
- * Zwraca:
- *    Sume dwoch skladnikow przekazanych jako parametry.
- */
-LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2){
-  LZespolona  Wynik;
-
-  Wynik.re = Skl1.re + Skl2.re;
-  Wynik.im = Skl1.im + Skl2.im;
-  return Wynik;
+LZespolona  operator - (LZespolona  liczba1,  LZespolona  liczba2)
+{
+  liczba1.re -= liczba2.re;
+  liczba1.im -= liczba2.im;
+  return liczba1;
 }
 
+LZespolona  operator * (LZespolona  liczba1,  LZespolona  liczba2)
+{
+  LZespolona wynik;
+  wynik.re = (liczba1.re*liczba2.re)-(liczba1.im*liczba2.im);
+  wynik.im = (liczba1.re*liczba2.im)+(liczba1.im*liczba2.re);
+  return wynik;
+}
 
-/*!
- * Realizuje dzielenie liczby zespolonej przez skakar.
- * Argumenty:
- *    Skl1 - dzielona liczba zespolona,
- *    Skl2 - skalar-dzielnik.
- * Zwraca:
- *    Wynik dzielenia dwoch skladnikow przekazanych jako parametry.
- */
-LZespolona  operator / (LZespolona  Skl1,  double  Skl2){
-  LZespolona  Wynik;
+LZespolona  operator * (LZespolona  liczba1,  double  liczba2)
+{
+  liczba1.re *= liczba2;
+  liczba1.im *= liczba2;
+  return liczba1;
+}
 
-  Wynik.re = Skl1.re / Skl2;
-  Wynik.im = Skl1.im / Skl2;
-  return Wynik;
+LZespolona operator * (double liczba1, LZespolona liczba2)
+{
+  return liczba2 * liczba1;
+}
+
+LZespolona  operator / (LZespolona  liczba1,  LZespolona  liczba2)
+{
+  double modul2 = liczba2.Modul2();
+  if(modul2 == 0)
+    throw (1);
+  return (liczba1 * liczba2.Sprzezenie()) / modul2;
+}
+
+LZespolona  operator / (LZespolona  liczba1,  double  liczba2)
+{
+  if(liczba2 == 0)
+    throw (1);
+  liczba1.re /= liczba2;
+  liczba1.im /= liczba2;
+  return liczba1;
+}
+
+bool operator == (LZespolona  liczba1,  LZespolona  liczba2)
+{
+  if(liczba1.re != liczba2.re)
+    return false;
+  if(liczba1.im != liczba2.im)
+    return false;
+  return true;
+}
+
+bool operator != (LZespolona  liczba1,  LZespolona  liczba2)
+{
+  return !(liczba1 == liczba2);
+}
+
+std::ostream & operator << ( std::ostream & s, LZespolona liczba)
+{
+  // Wyświetlanie części rzeczywistej
+  if(liczba.re == 0)
+    s << "(0";
+  else
+    s << "(" << liczba.re;
+  // Wyświetlanie części urojonej
+  if(liczba.im > 0)
+    return s << "+" << liczba.im << "i)";
+  if(liczba.im == 0)
+    return s  << "+0i)";
+  return s << liczba.im << "i)";
+}
+
+std::istream & operator >> (std::istream& is, LZespolona& liczba)
+{
+  char nawias, litera;
+  // Pobieranie nawiasu
+  is >> nawias;
+  if(is.fail())
+    return is;
+  if(nawias != '(')
+  {
+    is.setstate(std::ios::failbit);
+    return is;
+  }
+  // Pobieranie części rzeczywistej
+  is >> liczba.re;
+  if(liczba.re == 0)
+    liczba.re = 0;
+  if(is.fail())
+    return is;
+  // Pobieranie części urojonej
+  is >> liczba.im;
+  if(liczba.im == 0)
+    liczba.im = 0;
+  if(is.fail())
+    return is;
+  // Pobieranie litery 'i'
+  is >> litera;
+  if(is.fail())
+    return is;
+  if(litera != 'i')
+  {
+    is.setstate(std::ios::failbit);
+    return is;
+  }
+  // Pobieranie nawiasu
+  is >> nawias;
+  if(is.fail())
+    return is;
+  if(nawias != ')')
+  {
+    is.setstate(std::ios::failbit);
+    return is;
+  }
+  return is;
+}
+
+LZespolona::LZespolona()
+{
+  this->re = 0;
+  this->im = 0;
+}
+
+LZespolona::LZespolona(double reValue, double imValue)
+{
+  this->re = reValue;
+  this->im = imValue;
+}
+
+LZespolona LZespolona::Sprzezenie()
+{
+  LZespolona liczba;
+  liczba.re = this->re;
+  if(this->im != 0)                             // Zabezpieczenie się przed ustawieniem wartości części urojonej na -0
+    liczba.im = -this->im;
+  else
+    liczba.im = 0;
+  return liczba;
+}
+
+double LZespolona::Modul2()
+{
+  return (this->re*this->re) + (this->im*this->im);
 }
