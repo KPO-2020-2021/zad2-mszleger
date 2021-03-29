5b5e041d772cac5a47b0c7de2e166db7e0b15e42
Michał Szleger <259235@student.pwr.edu.pl>
2021-03-30 01:19:13 +0200

    Add support of incomplete complex number value.

--- src/LZespolona.cpp
+++ src/LZespolona.cpp
@@ -66,7 +66,7 @@ bool operator != (LZespolona  liczba1,  LZespolona  liczba2)
   return !(liczba1 == liczba2);
 }
 
-std::ostream & operator << ( std::ostream & s, LZespolona liczba)
+std::ostream & operator << (std::ostream & s, LZespolona liczba)
 {
   // Wyświetlanie części rzeczywistej
   if(liczba.re == 0)
@@ -83,6 +83,7 @@ std::ostream & operator << ( std::ostream & s, LZespolona liczba)
 
 std::istream & operator >> (std::istream& is, LZespolona& liczba)
 {
+  /*
   char nawias, litera;
   // Pobieranie nawiasu
   is >> nawias;
@@ -123,6 +124,141 @@ std::istream & operator >> (std::istream& is, LZespolona& liczba)
     is.setstate(std::ios::failbit);
     return is;
   }
+  return is;
+  //*/
+  char znak, peek1, peek2;
+  double liczba1, liczba2;
+  bool odczyt1 = false, odczyt2 = false;
+
+  // Pobieranie nawiasu
+  is >> znak;
+  if(is.fail())
+    return is;
+  if(znak != '(')
+  {
+    is.setstate(std::ios::failbit);
+    return is;
+  }
+
+  // Zapisywanie następnego znaku
+  peek1 = is.peek();
+  // Wczytywanie liczby nr. 1
+  is >> liczba1;
+  // Sprawdzanie czy udało się wczytać liczbę
+  if(is.fail() == false)
+  {
+    odczyt1 = true;
+    // Zapisywanie następnego znaku
+    peek2 = is.peek();
+    // Wczytywanie liczby nr. 2
+    is >> liczba2;
+    // Sprawdzanie czy udało się wczytać liczbę
+    if(is.fail() == false)
+    {
+      odczyt2 = true;
+    }
+  }
+  // Czyszczenie flagi błędu jeśli jest ustawiona
+  if(is.fail())
+    is.clear();
+
+  // Wczytywanie znaku
+  is >> znak;
+
+  // Zwracanie błędu jeśli nie udało się wczytać znaku
+  if(is.fail())
+    return is;
+
+  // Sprawdzanie czy wczytany znak to 'i'
+  if(znak == 'i')
+  {
+    // Wczytany znak to 'i'
+    // Sprawdzanie czy udało się wczytać obie liczby
+    if(odczyt1 && odczyt2)
+    {
+      liczba.re = liczba1;
+      liczba.im = liczba2;
+    }else{
+      // Sprawdzanie czy udało się wczytać pierwszą liczbę
+      if(odczyt1)
+      {
+        // Sprawdzanie czy podjerzany znak to '+'
+        if(peek2 == '+')
+        {
+          liczba.re = liczba1;
+          liczba.im = 1;
+        // Sprawdzanie czy podjerzany znak to '-'
+        }else if(peek2 == '-')
+        {
+          liczba.re = liczba1;
+          liczba.im = -1;
+        // Sprawdzanie czy podjerzany znak to 'i'
+        }else if(peek2 == 'i')
+        {
+          liczba.re = 0;
+          liczba.im = liczba1;
+        }else{
+          // Zwracanie błędu jeśli nie rozpoznano znaku
+          is.setstate(std::ios::failbit);
+          return is;
+        }
+      }else{
+        // Sprawdzanie czy podjerzany znak to '+'
+        if(peek1 == '+')
+        {
+          liczba.re = 0;
+          liczba.im = 1;
+        // Sprawdzanie czy podjerzany znak to '-'
+        }else if(peek1 == '-')
+        {
+          liczba.re = 0;
+          liczba.im = -1;
+        // Sprawdzanie czy podjerzany znak to 'i'
+        }else if(peek1 == 'i')
+        {
+          liczba.re = 0;
+          liczba.im = 1;
+        }else{
+          // Zwracanie błędu jeśli nie rozpoznano znaku
+          is.setstate(std::ios::failbit);
+          return is;
+        }
+      }
+    }
+    // Wczytywanie znaku
+    is >> znak;
+    // Zwracanie błędu jeśli nie udało się wczytać znaku
+    if(is.fail())
+      return is;
+  }else{
+    // Wczytany znak to nie 'i'
+    // Sprawdzanie czy udało się wczytać obie liczby
+    if(odczyt1 && odczyt2)
+    {
+      // Zwracanie błędu
+      is.setstate(std::ios::failbit);
+      return is;
+    }else{
+      // Sprawdzanie czy udało się wczytać pierwszą liczbę
+      if(odczyt1)
+      {
+        liczba.re = liczba1;
+        liczba.im = 0;
+      }else{
+      // Zwracanie błędu
+      is.setstate(std::ios::failbit);
+      return is; 
+      }
+    }
+  }
+
+  // Sprawdzanie czy wczytano nawias
+  if(znak != ')')
+  {
+    is.setstate(std::ios::failbit);
+    return is;
+  }
+  // Poprawne kończenie pracy funkcji
   return is;
 }
 
